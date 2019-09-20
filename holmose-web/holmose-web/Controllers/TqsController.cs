17667fe50529f0eea3c6a7384c81254aa654ea2c
holmose <holmose@qq.com>
2019-09-20 15:37:02 +0800

    tqs

---
+++ holmose-web/holmose-web/Controllers/TqsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace holmose_web.Controllers
+{
+    public class TqsController : Controller
+    {
+        //
+        // GET: /Tqs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
